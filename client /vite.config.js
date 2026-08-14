a2d99355bc973c311affdea900f98fb11b598a47
antarbarua483 <antarbarua009@gmail.com>
2026-08-14 13:53:10 +0600

    Update vite.config.js

--- client /vite.config.js
+++ client /vite.config.js
@@ -1,1 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      // All /api/* requests forwarded to Express during development
+      '/api': {
+        target:      'http://localhost:5000',
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    sourcemap: true
+  }
+})
